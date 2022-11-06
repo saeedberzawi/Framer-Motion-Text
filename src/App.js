5e62dcd2526008443a253cad8650c2e25ce5a138
Saeed Al Berzawi <saidberzawi@gmail.com>
2022-11-06 19:34:07 +0300

    initail

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
+import "./sass/main.scss";
+
+// Components
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Loader from "./components/Loader";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loading
+      ? document.querySelector("body").classList.add("loading")
+      : document.querySelector("body").classList.remove("loading");
+  }, [loading]);
+
+  return (
+    <AnimateSharedLayout type="crossfade">
+      <AnimatePresence>
+        {loading ? (
+          <motion.div key="loader">
+            <Loader setLoading={setLoading} />
+          </motion.div>
+        ) : (
+          <>
+            <Header />
+            <Banner />
+            {!loading && (
+              <div className="transition-image final">
+                <motion.img
+                  transition={{ ease: [0.6, 0.01, -0.05, 0.9], duration: 1.6 }}
+                  src={process.env.PUBLIC_URL + `/images/image-2.jpg`}
+                  layoutId="main-image-1"
+                />
+              </div>
+            )}
+          </>
+        )}
+      </AnimatePresence>
+    </AnimateSharedLayout>
+  );
+}
+
+export default App;
